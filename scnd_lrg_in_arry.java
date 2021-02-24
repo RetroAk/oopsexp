c05eeb601a9b5f9d830c2ae11ef04d13badda558
RetroAk <60184725+RetroAk@users.noreply.github.com>
2021-02-24 18:27:17 +0530

    Update scnd_lrg_in_arry.java

--- scnd_lrg_in_arry.java
+++ scnd_lrg_in_arry.java
@@ -3,22 +3,17 @@ public class scnd_lrg_in_arry {
 public static void main(String[] args) {
 Scanner sc= new Scanner(System.in);
 System.out.println("Enter the number of elements in the integer array:");int noOfElements = sc.nextInt();
-int [] array = new int[noOfElements];
+int array[] = new int[noOfElements];
 System.out.println("Enter the "+noOfElements+" elements:");
 for(int i=0;i<noOfElements;i++) {
 array[i] = sc.nextInt();
 }
-int largest =array[0];
-int secondLargest = array[0];
-for (int i = 0; i < array.length; i++) {
-if (array[i] > largest) {
-secondLargest = largest;
-largest = array[i];
-} else if (array[i] > secondLargest) {
-secondLargest = array[i];
-}//end if-else if
-}//end for
-System.out.println("\nSecond largest number is:" + secondLargest);
-sc.close();
-}//end main
+int temp;                                                                                       for(int i = 0; i<array.length; i++ ){
+         for(int j = i+1; j<array.length; j++){                                                 
+            if(array[i]>array[j]){                                                                             temp = array[i];
+               array[i] = array[j];
+               array[j] = temp;}}}
+
+System.out.println("\nSecond largest number is:" + array[array.length-2]);
+sc.close();}
 }
